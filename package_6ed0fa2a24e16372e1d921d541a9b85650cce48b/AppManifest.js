eae74431411e7b7bd00a74d487a1010ae3c51b93
trungnt2910 <trungnt2910@users.noreply.github.com>
2022-11-21 09:54:04 +0000

    deploy: 3ad1408f5a8f1f12039f8166512211c4ff878f7e

---
+++ package_6ed0fa2a24e16372e1d921d541a9b85650cce48b/AppManifest.js
@@ -0,0 +1,15 @@
+﻿var UnoAppManifest = {
+
+    splashScreenImage: "Assets/CodeHashifier.svg",
+    splashScreenColor: "transparent",
+    displayName: "CodeHashifier Online"
+
+}
+
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
+link.href = config.uno_app_base + '/Assets/CodeHashifier.ico';
